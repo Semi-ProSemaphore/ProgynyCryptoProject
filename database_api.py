c0ef2c5188554562ae9fa0bb06abe2d33f3fd932
Milen <tjurren@gmail.com>
2022-07-06 21:43:38 -0400

    Adding holdings, coin, and coin history db functions.

--- database_api.py
+++ database_api.py
@@ -1,6 +1,7 @@
 from typing import List, Dict
 from schema_objects import Portfolio, Coin
 from dotenv import load_dotenv
+from datetime import datetime
 import os
 import pymysql
 
@@ -17,7 +18,7 @@ class Database:
 
     def is_existing_portfolio(self, portfolio_id: int) -> bool:
         _cursor = self.conn.cursor()
-        _cursor.execute("SELECT * FROM Portfolio WHERE ID = {0}".format(portfolio_id))
+        _cursor.execute("SELECT * FROM Portfolio WHERE ID = {0};".format(portfolio_id))
 
         results = _cursor.fetchall()
         _cursor.close()
@@ -25,7 +26,7 @@ class Database:
 
     def get_portfolio(self, portfolio_id: int) -> Portfolio:
         _cursor = self.conn.cursor()
-        _cursor.execute("SELECT * FROM Portfolio WHERE ID = {0}".format(portfolio_id))
+        _cursor.execute("SELECT * FROM Portfolio WHERE ID = {0};".format(portfolio_id))
 
         results = _cursor.fetchall()
         _cursor.close()
@@ -34,7 +35,7 @@ class Database:
     def create_portfolio(self, cash_balance: float) -> Portfolio:
         _cursor = self.conn.cursor()
         _cursor.execute("INSERT INTO Portfolio (CASH_BALANCE) VALUES ({0});".format(cash_balance))
-        _cursor.execute("SELECT * FROM Portfolio WHERE ID = LAST_INSERT_ID()")
+        _cursor.execute("SELECT * FROM Portfolio WHERE ID = LAST_INSERT_ID();")
 
         results = _cursor.fetchall()
         _cursor.close()
@@ -43,7 +44,7 @@ class Database:
     def update_portfolio(self, portfolio_id: int, less_cash: float) -> Portfolio:
         _cursor = self.conn.cursor()
         _cursor.execute("UPDATE Portfolio SET CASH_BALANCE = CASH_BALANCE - {0};".format(less_cash))
-        _cursor.execute("SELECT * FROM Portfolio WHERE ID = {0}".format(portfolio_id))
+        _cursor.execute("SELECT * FROM Portfolio WHERE ID = {0};".format(portfolio_id))
 
         results = _cursor.fetchall()
         _cursor.close()
@@ -51,15 +52,88 @@ class Database:
 
     # This call may need to be revised.
     def get_portfolio_holdings(self, portfolio_id: int) -> Dict[str, float]:
-        pass
+        _cursor = self.conn.cursor()
+        _cursor.execute("""SELECT c.ALTERNATE_ID, h.BALANCE 
+                        FROM Holdings h 
+                        LEFT JOIN Coin c ON c.ID = h.COIN_ID 
+                        WHERE PORTFOLIO_ID = {0};"""
+                        .format(portfolio_id))
+
+        results = _cursor.fetchall()
+        _cursor.close()
+        holdings = {}
+        for entry in results:
+            print("entry: ", entry)
+            holdings[entry[0]] = entry[1]
+        return holdings
+
+    def is_coin_in_portfolio_holdings(self, portfolio_id: int, alternate_coin_id: str) -> bool:
+        _cursor = self.conn.cursor()
+        _cursor.execute("""SELECT * FROM Holdings WHERE PORTFOLIO_ID = {0} AND COIN_ID = 
+                        (SELECT ID FROM Coin WHERE ALTERNATE_ID = '{1}');"""
+                        .format(portfolio_id, alternate_coin_id))
+
+        results = _cursor.fetchall()
+        _cursor.close()
+        return True if results else False
+
+    def add_to_portfolio_holdings(self, portfolio_id: int, alternate_coin_id: str, amount: float) -> None:
+        _cursor = self.conn.cursor()
+        # _cursor.execute("SELECT ID FROM Coin WHERE ALTERNATE_ID = '{0}';".format(alternate_coin_id))
+
+        # db_coin = _cursor.fetchall()
+        _cursor.execute("""INSERT INTO Holdings (PORTFOLIO_ID, COIN_ID, BALANCE) 
+                        VALUES ({0},(SELECT ID FROM Coin WHERE ALTERNATE_ID = '{1}'),{2});"""
+                        .format(portfolio_id, alternate_coin_id, amount))
+        _cursor.close()
 
     def update_portfolio_holdings(self, portfolio_id: int, alternate_coin_id: str, change: float) -> None:
-        pass
+        _cursor = self.conn.cursor()
+        _cursor.execute("""UPDATE Holdings 
+                        SET BALANCE = BALANCE + {0} 
+                        WHERE PORTFOLIO_ID = {1} 
+                        AND COIN_ID = (
+                            SELECT ID FROM Coin WHERE ALTERNATE_ID = '{2}'
+                        );"""
+                        .format(change, portfolio_id, alternate_coin_id))
+        _cursor.close()
 
     # This call may also need to be revised.
-    def get_coin_history(self, alternate_coin_id: str) -> List[str]:
-        pass
+    def get_coin_history(self, alternate_coin_id: str) -> List[List[str]]:
+        _cursor = self.conn.cursor()
+        _cursor.execute("""SELECT * 
+                        FROM HistoricalCoinData 
+                        WHERE COIN_ID = (
+                            SELECT ID FROM Coin WHERE ALTERNATE_ID = '{0}
+                        );"""
+                        .format(alternate_coin_id))
+        results = _cursor.fetchall()
+        _cursor.close()
+
+        return [list(result) for result in results]
 
     # Entire set of coin information is not needed yet, but may be in the future. Thus adding Coin obj.
     def add_coin_history(self, coin: Coin) -> None:
-        pass
+        _cursor = self.conn.cursor()
+        _cursor.execute("""INSERT INTO HistoricalCoinData 
+                        (COIN_ID, CURRENT_PRICE, DATE) 
+                        VALUES ((
+                            SELECT ID FROM Coin WHERE ALTERNATE_ID = '{0}')
+                            ,{1},'{2}');"""
+                        .format(
+                            coin.id, coin.current_price, datetime.strptime(coin.date, "%Y-%m-%dT%H:%M:%S.%fZ")))
+        _cursor.close()
+
+    def is_existing_coin(self, coin: Coin) -> bool:
+        _cursor = self.conn.cursor()
+        _cursor.execute("SELECT * FROM Coin WHERE ALTERNATE_ID = '{0}';".format(coin.id))
+
+        results = _cursor.fetchall()
+        _cursor.close()
+        return True if results else False
+
+    def create_coin(self, coin: Coin) -> None:
+        _cursor = self.conn.cursor()
+        _cursor.execute("INSERT INTO Coin (ALTERNATE_ID, SYMBOL, NAME) VALUES ('{0}','{1}','{2}');".format(
+            coin.id, coin.symbol, coin.name))
+        _cursor.close()
